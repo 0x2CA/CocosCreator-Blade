95b730fd8de07b5eb125aae8771fb9f62cc7c799
0x2CA <2478557459@qq.com>
2023-06-02 19:21:06 +0800

    角色新增事件中心

--- assets/Scripts/Blade/Libs/FightSystem/Actor/CharacterBase.ts
+++ assets/Scripts/Blade/Libs/FightSystem/Actor/CharacterBase.ts
@@ -37,6 +37,11 @@ export default abstract class CharacterBase<
      */
     protected _skillDealWith: SD = null;
 
+    /**
+     * 角色事件
+     */
+    private _event: cc.EventTarget = new cc.EventTarget();
+
     public constructor(id: string) {
         super(id);
     }
@@ -326,6 +331,35 @@ export default abstract class CharacterBase<
      * @param delta
      */
     protected abstract onTick(delta: number);
+
+
+    public hasEventListener(type: string): boolean {
+        return this._event.hasEventListener(type);
+    }
+
+    public emit(key: any, arg1?: any, arg2?: any, arg3?: any, arg4?: any, arg5?: any): void {
+        this._event.emit(key, arg1, arg2, arg3, arg4, arg5);
+    }
+
+    public on<T extends Function>(type: any, callback: T, target?: any, useCapture?: boolean): T {
+        return this._event.on(type, callback, target, useCapture);
+    }
+
+    public off(type: any, callback?: Function, target?: any): void {
+        this._event.off(type, callback, target);
+    }
+
+    public targetOff(target: any): void {
+        this._event.targetOff(target);
+    }
+
+    public once(type: any, callback: (arg1?: any, arg2?: any, arg3?: any, arg4?: any, arg5?: any) => void, target?: any): void {
+        this._event.once(type, callback, target);
+    }
+
+    public clearEvent(): void {
+        this._event.clear();
+    }
 }
 
 import AttributeBase from "../AttributeBase";
