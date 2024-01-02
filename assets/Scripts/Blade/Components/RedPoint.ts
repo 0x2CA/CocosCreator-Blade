f9de1e18a75b71b40028055ea954e72c1d415250
0x2CA <2478557459@qq.com>
2024-01-02 18:52:43 +0800

    修复红点

--- assets/Scripts/Blade/Components/RedPoint.ts
+++ assets/Scripts/Blade/Components/RedPoint.ts
@@ -12,7 +12,7 @@ export default class RedPoint extends ViewBase {
 
     private redPointIcon: string = "shop_bg_num02.png";
     private startScale = 1;
-    private startPosY = 0;
+    private startOffsetY = 0;
 
     private _isInit: boolean = false;
 
@@ -28,7 +28,7 @@ export default class RedPoint extends ViewBase {
 
     protected onShow() {
         if (this._isInit == true) {
-            this.showAnimation();
+            this.setState(this._state);
         }
     }
 
@@ -40,8 +40,10 @@ export default class RedPoint extends ViewBase {
 
     //设置红点位置
     public setRedPointPosition(pos: cc.Vec2) {
-        this.redPointNode.setPosition(pos);
-        this.startPosY = this.redPointNode.y;
+        // this.redPointNode.setPosition(pos);
+        this.redPointNode.x = pos.x;
+        this.redPointNode.y = pos.y;
+        this.startOffsetY = pos.y;
     }
 
     //设置红点icon
@@ -51,44 +53,66 @@ export default class RedPoint extends ViewBase {
         }
     }
 
+    private _state: boolean = false;
+
     //设置红点状态
-    public setRedPointState(state: boolean = false, pos: cc.Vec2 = new cc.Vec2(0, 0)) {
-        if (this.redPointNode == null) {
-            this.redPointNode = new cc.Node("redPoint");
-            this.node.addChild(this.redPointNode);
-            let sp = this.redPointNode.getComponent(cc.Sprite);
-            if (!sp) {
-                this.redPointNode.addComponent(cc.Sprite);
-            }
-            let nodeSize = this.node.getContentSize();
-            this.setRedPointPosition(new cc.Vec2(nodeSize.width * 0.5 + pos.x, nodeSize.height * 0.5 + pos.y));
-        }
-        if (this.redPointNode) {
-            this.setRedPointIcon(this.redPointIcon);
-            this.redPointNode.active = state;
-            this.redPointNode.scale = 1 / this.node.scale;
-            this.startScale = 1 / this.node.scale;
-            this.startPosY = this.redPointNode.y;
-            if (state) {
-                if (this._isInit == true) {
-                    this.showAnimation();
+    public setRedPointState(state: boolean = false, offset: cc.Vec2 = new cc.Vec2(0, 0)) {
+        if (cc.isValid(this, true)) {
+            this._state = state;
+            if (this.redPointNode == null) {
+                this.redPointNode = new cc.Node("redPoint");
+                this.node.addChild(this.redPointNode);
+                let sp = this.redPointNode.getComponent(cc.Sprite);
+                if (!sp) {
+                    this.redPointNode.addComponent(cc.Sprite);
                 }
             }
-            else {
-                blade.tween.removeTweens(this.redPointNode);
+            if (this.redPointNode != null) {
+                this.redPointNode.active = state;
+                this.redPointNode.scale = 1 / this.node.scale;
+                this.startScale = 1 / this.node.scale;
+                let nodeSize = this.node.getContentSize();
+                this.setRedPointPosition(new cc.Vec2(
+                    nodeSize.width * (1 - this.node.anchorX) + offset.x,
+                    nodeSize.height * (1 - this.node.anchorY) + offset.y
+                ));
+            }
+            if (this._isInit == true) {
+                this.setState(state);
+            }
+        }
+    }
+
+    private setState(state: boolean = false) {
+        if (cc.isValid(this, true)) {
+            if (this._isInit == true) {
+                if (this.redPointNode != null) {
+                    this.setRedPointIcon(this.redPointIcon);
+                    if (state) {
+                        this.showAnimation();
+                    }
+                    else {
+                        blade.tween.removeTweens(this.redPointNode);
+                    }
+                }
             }
         }
     }
 
     private showAnimation() {
-        if (this.redPointNode) {
-            blade.tween.removeTweens(this.redPointNode);
-            if (this.isValid == true) {
-                blade.tween.get(this.redPointNode)
-                    .set({ scaleY: this.startScale, y: this.startPosY })
-                    .to({ scaleY: this.startScale * 1.2, y: this.startPosY + 5 }, 600)
-                    .to({ scaleY: this.startScale, y: this.startPosY }, 600, Tween.Easing.Back.Out)
-                    .setLoop(true, true);
+        if (cc.isValid(this, true)) {
+            if (this._isInit == true) {
+                if (this.redPointNode != null) {
+                    blade.tween.removeTweens(this.redPointNode);
+                    let tween = blade.tween.get(this.redPointNode)
+                        .set({ scaleY: this.startScale, y: this.startOffsetY })
+                        .to({ scaleY: this.startScale * 1.2, y: this.startOffsetY + 5 }, 600)
+                        .to({ scaleY: this.startScale, y: this.startOffsetY }, 600, Tween.Easing.Back.Out);
+
+                    tween.setLoop(true, true);
+
+                    console.log(this.startOffsetY, tween, this.uuid)
+                }
             }
         }
     }
